0b61e4e0e1c585d61a8fbebc76b72730ee75d94d
Ali Irani <aliirani74@gmail.com>
2016-12-02 00:45:03 +0330

    step 3 done.

--- 2016/fall/HW-3/minesweeper.js
+++ 2016/fall/HW-3/minesweeper.js
@@ -65,12 +65,11 @@ function getGameXml2() {
 
 function makeXSL() {
     var xml= `
-        <?xml version="1.0" encoding="UTF-8"?>
         <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
             <xsl:template match="/grid/row/col">
-                <span row='{../@row}'
-                        col='{@col}'
-                        mine='{@mine}'></span>
+                <span row="{../@row}"
+                        col="{@col}"
+                        mine="{@mine}"></span>
             </xsl:template>
         </xsl:stylesheet>
         `;
